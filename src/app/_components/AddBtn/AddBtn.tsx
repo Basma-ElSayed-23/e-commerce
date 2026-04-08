17ece29853f457901996d7f392ee6b5596b24f17
Basma-ElSayed-23 <basmaezz02@gmail.com>
2026-04-08 20:15:28 +0200

    added new updates

--- src/app/_components/AddBtn/AddBtn.tsx
+++ src/app/_components/AddBtn/AddBtn.tsx
@@ -2,17 +2,27 @@
 import React from 'react'
 import { Button } from '@base-ui/react';
 import { addToCart } from '@/actions/cart.action';
+import toast from 'react-hot-toast';
 
-export default function AddBtn() {
+export default function AddBtn({classes, word , id }: {classes: string; word: string; id: string}) {
 
-  function AddProduct(){
+ async function AddProduct(){
  console.log("add");
- addToCart("")
+
+
+ const res = await addToCart(id);
+ if (res.status === "success") {
+  toast.success(res.message , {position: "top-right" , duration: 2000});
+ } else {
+  toast.error(res.message , {position: "top-right" , duration: 2000});
+ }
   }
 
   return (
     <>
-    <Button onClick={(e) => {e.preventDefault(); AddProduct();}} className='size-10 w-full rounded-lg bg-[#16A34A] text-white text-sm cursor-pointer p-4 my-4 font-bold '>Add to Cart</Button>
+    <Button onClick={(e) => {e.preventDefault(); AddProduct();}} className={classes} >{word}</Button>
     </>
-  )
+  );
 }
+
+//className='size-10 w-full rounded-lg bg-[#16A34A] text-white text-sm cursor-pointer p-4 my-4 font-bold '
